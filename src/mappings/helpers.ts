3e00739721eac4a8f7e813d58d66139636bb7364
Gabriel Nicolás González <gabrielnicolasgonzalez@gmail.com>
2021-12-24 18:10:02 -0300

    extract method to createPeriodsForContract function

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,5 +1,6 @@
-import { BigInt } from '@graphprotocol/graph-ts'
-import { GraphCirculatingSupply } from '../../generated/schema'
+import { Address, BigInt, log } from '@graphprotocol/graph-ts'
+import { GraphCirculatingSupply, ReleasePeriod } from '../../generated/schema'
+import { GraphTokenLockWallet } from '../../generated/templates'
 
 export function createOrLoadGraphCirculatingSupply(): GraphCirculatingSupply {
   let graphCirculatingSupply = GraphCirculatingSupply.load('1')
@@ -16,4 +17,46 @@ export function createOrLoadGraphCirculatingSupply(): GraphCirculatingSupply {
     graphCirculatingSupply.save()
   }
   return graphCirculatingSupply as GraphCirculatingSupply
+}
+
+export function createPeriodsForContract(contractAddress: Address, endTime: BigInt, startTime: BigInt, periods: BigInt, managedAmount: BigInt): void {
+  let graphCirculatingSupply = createOrLoadGraphCirculatingSupply()
+  let id = contractAddress.toHexString()
+  let releaseDuration = endTime.minus(startTime)
+  let periodsDuration = releaseDuration.div(periods)
+  let periodReleaseDate = startTime
+  let periodAmount = managedAmount.div(periods)
+  let periodsI32 = periods.toI32()
+
+  log.warning('[RELEASE PERIODS] creating release periods for contract: {}', [id])
+  for(let i = 0; i < periodsI32; i++) {
+    let periodId = id + "-" + i.toString();
+    let releasePeriod = new ReleasePeriod(periodId);
+    let periodsToProcess = graphCirculatingSupply.periodsToProcess
+    periodReleaseDate = periodReleaseDate.plus(periodsDuration)
+    releasePeriod.releaseDate = periodReleaseDate
+    releasePeriod.amount = periodAmount
+    releasePeriod.contract = id
+    releasePeriod.processed = false
+    releasePeriod.save()
+
+    if (i == 0){
+      if(graphCirculatingSupply.minPeriodToProcessDate.isZero() || 
+      graphCirculatingSupply.minPeriodToProcessDate > periodReleaseDate) {
+        graphCirculatingSupply.minPeriodToProcessDate = periodReleaseDate
+      }
+    }
+
+    periodsToProcess.push(periodId)
+    graphCirculatingSupply.periodsToProcess = periodsToProcess
+    graphCirculatingSupply.periodsToProcessTotalAmount = graphCirculatingSupply.periodsToProcessTotalAmount.plus(periodAmount)
+  }
+
+  let prevCirculatingSupply = graphCirculatingSupply.circulatingSupply
+
+  graphCirculatingSupply.circulatingSupply = prevCirculatingSupply.minus(managedAmount)
+  graphCirculatingSupply.save()
+
+  GraphTokenLockWallet.create(contractAddress)
+
 }
--- src/mappings/tokenLockWallets/manager.ts
+++ src/mappings/tokenLockWallets/manager.ts
@@ -1,47 +1,13 @@
 import { log } from '@graphprotocol/graph-ts'
 import { TokenLockCreated } from '../../../generated/GraphTokenLockManager/GraphTokenLockManager'
-import { ReleasePeriod } from '../../../generated/schema'
-import { createOrLoadGraphCirculatingSupply } from '../helpers'
-import { GraphTokenLockWallet } from '../../../generated/templates'
+import { createPeriodsForContract } from '../helpers'
 
 export function handleTokenLockCreated(event: TokenLockCreated): void {
-  let graphCirculatingSupply = createOrLoadGraphCirculatingSupply()
-  let id = event.params.contractAddress.toHexString()
-  let releaseDuration = event.params.endTime.minus(event.params.startTime)
-  let periodsDuration = releaseDuration.div(event.params.periods)
-  let periodReleaseDate = event.params.startTime
-  let periodAmount = event.params.managedAmount.div(event.params.periods)
-  let periods = event.params.periods.toI32()
+  let contractAddress = event.params.contractAddress
+  let endTime = event.params.endTime
+  let startTime = event.params.startTime
+  let periods = event.params.periods
+  let managedAmount = event.params.managedAmount
 
-  log.warning('[RELEASE PERIODS] creating release periods for contract: {}', [id])
-  for(let i = 0; i < periods; i++) {
-    let periodId = id + "-" + i.toString();
-    let releasePeriod = new ReleasePeriod(periodId);
-    let periodsToProcess = graphCirculatingSupply.periodsToProcess
-    periodReleaseDate = periodReleaseDate.plus(periodsDuration)
-    releasePeriod.releaseDate = periodReleaseDate
-    releasePeriod.amount = periodAmount
-    releasePeriod.contract = id
-    releasePeriod.processed = false
-    releasePeriod.save()
-
-    if (i == 0){
-      if(graphCirculatingSupply.minPeriodToProcessDate.isZero() || 
-      graphCirculatingSupply.minPeriodToProcessDate > periodReleaseDate) {
-        graphCirculatingSupply.minPeriodToProcessDate = periodReleaseDate
-      }
-    }
-
-    periodsToProcess.push(periodId)
-    graphCirculatingSupply.periodsToProcess = periodsToProcess
-    graphCirculatingSupply.periodsToProcessTotalAmount = graphCirculatingSupply.periodsToProcessTotalAmount.plus(periodAmount)
-  }
-
-  let prevCirculatingSupply = graphCirculatingSupply.circulatingSupply
-
-  graphCirculatingSupply.circulatingSupply = prevCirculatingSupply.minus(event.params.managedAmount)
-  graphCirculatingSupply.save()
-
-  GraphTokenLockWallet.create(event.params.contractAddress)
-
+  createPeriodsForContract(contractAddress, endTime, startTime, periods, managedAmount)
 }
