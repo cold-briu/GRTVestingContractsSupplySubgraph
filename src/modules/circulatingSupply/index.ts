d4c837c0d9002c0ba209de513f8cc19ebf12e034
Andrés Fernández <andresavena19@gmail.com>
2022-01-06 23:26:31 -0300

    feat: added mint and burn helpers

--- src/modules/circulatingSupply/index.ts
+++ src/modules/circulatingSupply/index.ts
@@ -20,10 +20,63 @@ export namespace circulatingSupply {
 			graphCirculatingSupply.periodsProcessed = periodsLists.constants.PROCESSED_LISTS_ID
 		}
 		return graphCirculatingSupply as GraphCirculatingSupply
+
+	}
+
+	export function mintTokens(
+		cs: GraphCirculatingSupply, value: BigInt
+	): GraphCirculatingSupply {
+		let graphCirculatingSupply = cs
+
+		graphCirculatingSupply = circulatingSupply.mutations.increaseTotalSupply(
+			graphCirculatingSupply, value
+		)
+
+		graphCirculatingSupply = circulatingSupply.mutations.increaseCirculatingSupply(
+			graphCirculatingSupply, value
+		)
+
+		return graphCirculatingSupply
+	}
+
+
+	export function burnTokens(
+		cs: GraphCirculatingSupply, value: BigInt
+	): GraphCirculatingSupply {
+		let graphCirculatingSupply = cs
+
+		graphCirculatingSupply = circulatingSupply.mutations.decreaseTotalSupply(
+			graphCirculatingSupply, value
+		)
+
+		graphCirculatingSupply = circulatingSupply.mutations.decreaseCirculatingSupply(
+			graphCirculatingSupply, value
+		)
+
+		return graphCirculatingSupply
 	}
 
 	export namespace mutations {
 
+		export function decreaseTotalSupply(
+			circulatingSupply: GraphCirculatingSupply, amount: BigInt
+		): GraphCirculatingSupply {
+			let cs = circulatingSupply
+			let supply = cs.totalSupply as BigInt
+			supply = supply.minus(amount)
+			cs.circulatingSupply = supply
+			return cs
+		}
+
+		export function increaseTotalSupply(
+			circulatingSupply: GraphCirculatingSupply, amount: BigInt
+		): GraphCirculatingSupply {
+			let cs = circulatingSupply
+			let supply = cs.totalSupply as BigInt
+			supply = supply.plus(amount)
+			cs.circulatingSupply = supply
+			return cs
+		}
 		export function decreaseCirculatingSupply(
 			circulatingSupply: GraphCirculatingSupply, amount: BigInt
 		): GraphCirculatingSupply {
@@ -54,7 +107,6 @@ export namespace circulatingSupply {
 			)
 			cs.minPeriodToProcessDate = minDate
 			return cs
-
 		}
 
 	}
