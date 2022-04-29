d7003dd02541287a47329818270e80ccc0ecb7c6
Daniel Russian <danielrussian@gmail.com>
2022-04-29 14:23:30 -0300

    Removing unnecessary log comments

--- src/mappings/tokenLockWallets/common.ts
+++ src/mappings/tokenLockWallets/common.ts
@@ -2,15 +2,11 @@ import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { GraphTokenLockWallet } from "../../../generated/templates"
 import { lockWalletContracts } from "../../modules"
 import { createPeriodsForContract } from "../mappingHelpers"
-import { log } from '@graphprotocol/graph-ts'
 
 export namespace common {
   const firstBLock: BigInt = BigInt.fromI32(11446769)
 
   export function isFirstBlock(blockNumber: BigInt): bool {
-    log.warning(": FIRST BLOCK : {}", [firstBLock.toString()])
-    log.warning(": ASKED BLOCK : {} - RESULT {}", [blockNumber.toString(), firstBLock == blockNumber ? 'true' : 'false'])
-
     return firstBLock == blockNumber
   }
 
