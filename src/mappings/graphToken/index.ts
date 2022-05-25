f621b6e8ea70b443920ff3b7ecd190ccaef454bd
Andrés Fernández <andresavena19@gmail.com>
2022-05-25 16:22:28 -0500

    feat: updated graph token helper

--- src/mappings/graphToken/index.ts
+++ src/mappings/graphToken/index.ts
@@ -1,33 +1,33 @@
 import { Transfer } from '../../../generated/GraphToken/GraphToken'
-import { circulatingSupply } from '../../modules'
+import { grt as grtModule } from '../../modules'
 import { address } from "@protofire/subgraph-toolkit";
 import { onstart } from '../initializer'
 import { common } from '../tokenLockWallets/common';
 
 export function handleTransfer(event: Transfer): void {
-  if (common.isFirstBlock(event.block.number)) {
-    onstart.loadDefautlExchanges()
-  }
-  
+	if (common.isFirstBlock(event.block.number)) {
+		onstart.loadDefautlExchanges()
+	}
+
 	let isFromZero = address.isZeroAddress(event.params.from)
 	let isToZero = address.isZeroAddress(event.params.to)
 
 	// Check this condition to avoid load entity in regular transfers
 	if (isFromZero || isToZero) {
-		let graphCirculatingSupply = circulatingSupply.createOrLoadGraphCirculatingSupply()
+		let grt = grtModule.createOrLoadGrt()
 
 		if (isFromZero) {
-			graphCirculatingSupply = circulatingSupply.mintTokens(
-				graphCirculatingSupply, event.params.value
+			grt = grtModule.mintTokens(
+				grt, event.params.value
 			)
 
 		} else if (isToZero) {
-			graphCirculatingSupply = circulatingSupply.burnTokens(
-				graphCirculatingSupply, event.params.value
+			grt = grtModule.burnTokens(
+				grt, event.params.value
 			)
 		}
 
-		graphCirculatingSupply.save()
+		grt.save()
 	}
 
 }
